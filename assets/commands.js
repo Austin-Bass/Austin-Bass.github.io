852c5c5423107acf9c4e98b0eb3375a427bbe4dc
Austin-Bass <127161209+Austin-Bass@users.noreply.github.com>
2023-03-31 17:10:03 -0300

    Initial commit

---
+++ assets/commands.js
@@ -0,0 +1,73 @@
+var linkedin = "https://www.linkedin.com/in/agustin-bassano/";
+var instagram = "https://www.instagram.com/agus.bass13/";
+var github = "https://github.com/Austin-Bass";
+var email = 'mailto:agubassano@gmail.com';
+
+whois = [
+  "<br>",
+  "Hey, I'm Agustin!👋",
+  "I'm a software developer and mechatronic engineering student, who builds entertaining websites like this one",
+  "and he develops videogames in his spare time.",
+  "After graduating with a Bachelor's in Social Sciences, I started working in",
+  "Where I developed several social skills that helped me improve my web pages and applications.",
+  "While doing all of that, I focused on improving my frontend skills,",
+  "Studying more about videogames development and investing in the stock market.", 
+  "I am passionate about the world of videogames, manga and music.",
+  "That's why I try to feed my projects with a bit of that passion and that style., and it's",
+  "the best decision I ever made.",
+  "Now, I'm able to focus all my knowledge and tastes on these projects,",
+  "This portfolio is a sample of what I can do",
+  "with a little time and a little inspiration from the retro years of video games.",
+  "I hoped you enjoy it like i do.",
+  "<br>"
+];
+
+quote = [
+  "<br>",
+  "'If someone already did it, then go and innovate it.'",
+  "Agustin Bassano",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/agustin-bassano' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/agus.bass13' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/Austin-Bass' + "</a>",
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Agustin?',
+  '<span class="command">mind</span>           A personal quote',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You literally already used it',
+  '<span class="command">email</span>          Send me an email here anytime',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Agustin Bassano. All rights reserved.</span>',
+  "        .____.          ___      ___   __________   ____________    ____   ___          ___       ",
+  "       _|    |_        |   |    |   | |     _____| |            |  /    \  |   |        |   |",
+  "     _|        |_      |   |    |   | |    |_____  |___      ___|  \____/  |   |__      |   | ",
+  "    |  __    __  |     |   |    |   | |          |     |    |     |    |  |      |_    |   |     ",
+  "    | |__|  |__| |     |   |    |   | |______    |     |    |     |    |  |        |_  |   |  ",
+  " ._  |  __    __  |  _. |   |    |   |        |   |     |    |     |    |  |    __    |_|   | ", 
+  "|_|_|_|  |__|  |_|_|_| |   |____|   |   _____|   |     |    |     |    |  |   |  |__       | ",   
+  "  |_|_          _|_|   |            | |          |     |    |     |    |  |   |     |__    | ",
+  "    |_|        |_|     |____________| |__________|     |____|     |____|  |___|        |___|",
+  '<span class="color2">Welcome to my personal portfolio.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
---
+++ assets/index.js
@@ -0,0 +1,133 @@
+var before = document.getElementById("before");
+var command = document.getElementById("typer"); 
+var textarea = document.getElementById("texter"); 
+var terminal = document.getElementById("terminal");
+
+var git = 0;
+var pw = false;
+let pwd = false;
+var commands = [];
+
+setTimeout(function() {
+  loopLines(banner, "", 80);
+  textarea.focus();
+}, 100);
+
+window.addEventListener("keyup", enterKey);
+
+//init
+textarea.value = "";
+command.innerHTML = textarea.value;
+
+function enterKey(e) {
+    if (e.keyCode == 13) {
+      commands.push(command.innerHTML);
+      git = commands.length;
+      addLine("new@user:~$ " + command.innerHTML, "no-animation", 0);
+      commander(command.innerHTML.toLowerCase());
+      command.innerHTML = "";
+      textarea.value = "";
+    }
+    if (e.keyCode == 38 && git != 0) {
+      git -= 1;
+      textarea.value = commands[git];
+      command.innerHTML = textarea.value;
+    }
+    if (e.keyCode == 40 && git != commands.length) {
+      git += 1;
+      if (commands[git] === undefined) {
+        textarea.value = "";
+      } else {
+        textarea.value = commands[git];
+      }
+      command.innerHTML = textarea.value;
+    }
+  }
+
+
+function commander(cmd) {
+  switch (cmd.toLowerCase()) {
+    case "help":
+      loopLines(help, "color2 margin", 80);
+      break;
+    case "whois":
+      loopLines(whois, "color2 margin", 80);
+      break;
+    case "mind":
+      loopLines(quote, "color2 margin", 80);
+      break;
+    case "social":
+      loopLines(social, "color2 margin", 80);
+      break;
+    case "projects":
+      loopLines(projects, "color2 margin", 80);
+      break;
+    case "history":
+      addLine("<br>", "", 0);
+      loopLines(commands, "color2", 80);
+      addLine("<br>", "command", 80 * commands.length + 50);
+      break;
+    case "email":
+      addLine('Opening mailto:<a href="mailto:agubassano@gmail.com">agubassano@gmail.com</a>...', "color2", 80);
+      newTab(email);
+      break;
+    case "clear":
+      setTimeout(function() {
+        terminal.innerHTML = '<a id="before"></a>';
+        before = document.getElementById("before");
+      }, 1);
+      break;
+    case "banner":
+      loopLines(banner, "", 80);
+      break;
+    // socials
+    case "linkedin":
+      addLine("Opening LinkedIn...", "color2", 0);
+      newTab(linkedin);
+      break;
+    case "instagram":
+      addLine("Opening Instagram...", "color2", 0);
+      newTab(instagram);
+      break;
+    case "github":
+      addLine("Opening GitHub...", "color2", 0);
+      newTab(github);
+      break;
+    default:
+      addLine("<span class=\"inherit\">Command not found. For a list of commands, type <span class=\"command\">'help'</span>.</span>", "error", 100);
+      break;
+  }
+}
+
+function newTab(link) {
+  setTimeout(function() {
+    window.open(link, "_blank");
+  }, 500);
+}
+
+function addLine(text, style, time) {
+  var t = "";
+  for (let i = 0; i < text.length; i++) {
+    if (text.charAt(i) == " " && text.charAt(i + 1) == " ") {
+      t += "&nbsp;&nbsp;";
+      i++;
+    } else {
+      t += text.charAt(i);
+    }
+  }
+  setTimeout(function() {
+    var next = document.createElement("p");
+    next.innerHTML = t;
+    next.className = style;
+
+    before.parentNode.insertBefore(next, before);
+
+    window.scrollTo(0, document.body.offsetHeight);
+  }, time);
+}
+
+function loopLines(name, style, time) {
+  name.forEach(function(item, index) {
+    addLine(item, style, index * time);
+  });
+}
